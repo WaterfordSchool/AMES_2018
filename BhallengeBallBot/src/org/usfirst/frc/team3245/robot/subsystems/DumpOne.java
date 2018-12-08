775a0c826442de9b211fd54c30dc2d9de391c606
robotics <robotics@10.1.19.38>
2018-12-07 17:30:48 -0700

    We added commands for the first dump mechanism however it doesn't work right now

--- BhallengeBallBot/src/org/usfirst/frc/team3245/robot/subsystems/DumpOne.java
+++ BhallengeBallBot/src/org/usfirst/frc/team3245/robot/subsystems/DumpOne.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3245.robot.subsystems;
 
 import org.usfirst.frc.team3245.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -9,7 +10,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class DumpOne extends Subsystem {
-private final Talon dumpOne = RobotMap.DumpOne;
+private final Talon dumpOne = RobotMap.dumpOneMotor;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
@@ -17,13 +18,13 @@ private final Talon dumpOne = RobotMap.DumpOne;
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
- public void armDump(double dumpSpeed) {
-	 dumpOne.set(dumpSpeed);
-	 
+ public void armDump() {
+	 dumpOne.set(0.1);
  }
  
  public void stop () {
 	 dumpOne.set(0);
  }
+
 }
 
